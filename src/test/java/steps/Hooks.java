87d980e4b80660a966155c82485ce0b0ea5c8ed9
Your Name <you@example.comclear>
2022-08-24 16:43:29 -0400

    Test

---
+++ src/test/java/steps/Hooks.java
@@ -0,0 +1,34 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import utils.CommonMethods;
+
+public class Hooks extends CommonMethods {
+    //import io.cucumber and not junit
+
+    @Before
+    public void start(){
+        openBrowserAndLaunchApplication();
+    }
+
+    @After
+    public void end(Scenario scenario){
+
+        //scenario class from cucumber holds the complete information of the execution
+        byte[] pic;
+
+        if(scenario.isFailed()){
+            pic = takeScreenshot("failed/" + scenario.getName());
+        }else {
+            pic = takeScreenshot("passed/" + scenario.getName());
+        }
+
+        //it will attach the screenshot in report
+        scenario.attach(pic, "image/png", scenario.getName());
+
+        closeBrowser();
+    }
+
+}
